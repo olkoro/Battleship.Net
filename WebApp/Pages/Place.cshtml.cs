0dae1c905af86d355eb7e2e9ff20b6247d39e6c7
Oleg <olehik98@gmail.com>
2018-12-28 21:19:58 +0200

    use table in placing

--- WebApp/Pages/Place.cshtml.cs
+++ WebApp/Pages/Place.cshtml.cs
@@ -1,3 +1,4 @@
+using Domain;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApp.Pages
@@ -5,10 +6,13 @@ namespace WebApp.Pages
     public class Place : PageModel
     {
         public string gbString = "No GB";
+        public GameBoard GameBoard = new GameBoard(10,10);
         public void OnGetAsync()
         {
             WebUI.Run();
             gbString = WebUI.GetString();
+            GameBoard = WebUI.GetGameBoard()[0];
+
         }
     }
 }
