f9d1c24752932f532276eb6b4792c565366f11f4
Oleg <olehik98@gmail.com>
2019-01-04 07:53:42 +0200

    saving

--- WebApp/Pages/Game.cshtml.cs
+++ WebApp/Pages/Game.cshtml.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Domain;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -11,21 +12,42 @@ namespace WebApp.Pages
         public GameBoard Map;
         public Player Player;
         public string Status = null;
-        public void OnGet(string where)
+        public async Task OnGetAsync(string where)
         {
             Player = WebUI.Current;
             if (Player.AI)
             {
                 Status = WebUI.ShootAI();
             }
-            if (where == "go" | where == null)
+            if (where == "go" || where == null|| where =="save")
             {
                 //Status = WebUI.ShootAI();
+                if (where == "save")
+                {
+                    if (SaveSystem.GameStates.Count != 0)
+                    {
+                        if (SaveSystem.GameStates.Last().Status == null)
+                        {
+                            SaveSystem.GameStates.Last().Status = "[" + SaveSystem.GameStates.Last().P1.ToString() +
+                                                                  ": " + SaveSystem.GameStates.Last().P1.Board.Ships.Count +
+                                                                  " Ships, " +
+                                                                  SaveSystem.GameStates.Last().P2.ToString() +
+                                                                  ": " + SaveSystem.GameStates.Last().P2.Board.Ships.Count +
+                                                                  " Ships]";
+                        }
+
+                        SaveSystem.SavesList.Add(new List<State>(SaveSystem.GameStates));
+                
+                    }
+                    SaveSystem.GameStates = new List<State>();
+                    DAL.AppDbContext.SaveToDb();
+                }
             }
             else
             {
                 Status = WebUI.Shoot(where);
             }
+            SaveSystem.GameStates.Add(new State(WebUI.Player1,WebUI.Player2,Rules.CanTouch,WebUI.P2Turn));
             if (WebUI.GetWinner() == null){}
             else
             {
@@ -33,6 +55,7 @@ namespace WebApp.Pages
             }
             GameBoard = Player.Board;
             Map = Player.Map;
+            
         }
     }
 }
